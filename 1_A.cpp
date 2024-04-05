3537181f12a719c29578db72cc7f00b7d4ba696b
lorenzo.canovas <jlorenzocanovas@gmail.com>
2024-04-04 23:17:33 -0300

    Changing to a larger natural number type

--- 1_A.cpp
+++ 1_A.cpp
@@ -11,7 +11,7 @@ using namespace std;
 #define MAX_VALUE pow(10, 9)
 #define MIN_VALUE 1
 
-unsigned int division_with_rounding_up(unsigned int x, unsigned int y){
+unsigned long int division_with_rounding_up(unsigned long int x, unsigned long int y){
     unsigned int module_y = x % y;
 
     return x / y + (module_y != 0 ? 1 : 0);
@@ -20,11 +20,11 @@ unsigned int division_with_rounding_up(unsigned int x, unsigned int y){
 int main() {
     ios::sync_with_stdio(0);cin.tie(0);cout.tie(0);
 
-    unsigned int n = 0;
-    unsigned int m = 0;
-    unsigned int a = 0;
+    unsigned long int n = 0;
+    unsigned long int m = 0;
+    unsigned long int a = 0;
 
-    int try_scanf = scanf("%u %u %u", &n, &m, &a);
+    int try_scanf = scanf("%lu %lu %lu", &n, &m, &a);
     if(try_scanf != 3){
         printf("Error reading measurements\n");
         exit(EXIT_FAILURE);
@@ -35,14 +35,14 @@ int main() {
         || (MAX_VALUE < m || m < MIN_VALUE)
         || (MAX_VALUE < a || a < MIN_VALUE)
     ){
-        printf("The number of problems must be in the range 1 ≤ n ≤ 1000\n");
+        printf("The number of problems must be in the range 1 ≤ n, m, a ≤ 10^9\n");
         exit(EXIT_FAILURE);
     }
 
-    unsigned int flagstone_x = division_with_rounding_up(n, a);
-    unsigned int flagstone_y =division_with_rounding_up(m, a);
+    unsigned long int flagstone_x = division_with_rounding_up(n, a);
+    unsigned long int flagstone_y = division_with_rounding_up(m, a);
 
-    printf("%u\n", flagstone_x * flagstone_y);
+    printf("%lu\n", flagstone_x * flagstone_y);
 
     return EXIT_SUCCESS;
 }
