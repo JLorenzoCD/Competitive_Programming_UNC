64399e3bcf8c307de638e6fb162128a701fb63c8
lorenzo.canovas <jlorenzocanovas@gmail.com>
2024-04-09 23:09:07 -0300

    P - 800 - 281_A

---
+++ 800/281_A.cpp
@@ -0,0 +1,19 @@
+// https://codeforces.com/problemset/problem/281/A
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(void) {
+    ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+
+    string input;
+
+    cin >> input;
+
+    input[0] = toupper(input[0]);
+
+    cout << input << endl;
+
+    return EXIT_SUCCESS;
+}
