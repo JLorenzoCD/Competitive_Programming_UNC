ba04f4c94d67ee1f85c4326d24262b8f3f66ca80
lorenzo.canovas <jlorenzocanovas@gmail.com>
2024-04-12 23:04:56 -0300

    Update basic template

--- template/basic.cpp
+++ template/basic.cpp
@@ -4,6 +4,10 @@
 
 using namespace std;
 
+typedef long long ll;
+typedef unsigned long long ull;
+typedef long double ld;
+
 int main(void) {
     ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
 
