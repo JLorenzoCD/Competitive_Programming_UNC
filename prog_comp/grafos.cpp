420f8e32df225fd6e5a0a5c19466496b57f13d6b
lorenzo.canovas <jlorenzocanovas@gmail.com>
2024-05-14 18:45:36 -0300

    Programación competitiva - Tema - Grafos

---
+++ prog_comp/grafos.cpp
@@ -0,0 +1,64 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef long double ld;
+
+typedef unsigned int uint;
+
+void dfs(ll v, vector<vector<ll>> &g, vector<bool> &vis) {
+    vis[v] = true;
+
+    for (ll u : g[v]) {
+        if (!vis[u]) {
+            dfs(u, g, vis);
+        }
+    }
+}
+
+void bfs(ll v, vector<vector<ll>> &g) {
+    vector<bool> vis((ll)(g.size()));
+    queue<ll> q;
+
+    vis[v] = true;
+    q.push(v);
+
+    while (!q.empty()) {
+        v = q.front();
+        q.pop();
+
+        for (ll u : g[v]) {
+            if (!vis[u]) {
+                vis[u] = true;
+                q.push(u);
+            }
+        }
+    }
+}
+
+
+int main(void) {
+    ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+
+    // Leer grafos
+    ll n, m;
+    cin >> n >> m;
+    vector<vector<ll>> g(n);
+
+    for (ll i = 0; i < m; i++) {
+        ll u, v;
+        cin >> u >> v;
+
+        u--; v--;
+
+        g[u].push_back(v);
+        g[v].push_back(u);
+    }
+
+    // Llamar a bfs
+    bfs(0u, g);
+
+    return EXIT_SUCCESS;
+}
